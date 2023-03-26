b73684ba72a621c6296278ee2903a16a7a0fc9d3
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-26 18:56:30 -0500

    add op class

---
+++ app/src/main/java/com/example/intenttask/Operations.java
@@ -0,0 +1,19 @@
+package com.example.intenttask;
+
+public class Operations {
+    public int suma(int numOne, int numTwo) {
+        return numOne + numTwo;
+    }
+
+    public int resta(int numOne, int numTwo) {
+        return numOne - numTwo;
+    }
+
+    public int multiplicacion(int numOne, int numTwo) {
+        return numOne * numTwo;
+    }
+
+    public int division(int numOne, int numTwo) {
+        return numOne / numTwo;
+    }
+}
