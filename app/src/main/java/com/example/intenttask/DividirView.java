02c79c3c9fa9b92a0a1f6f2321f673f25add10b3
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-26 17:19:31 -0500

    add all the views

---
+++ app/src/main/java/com/example/intenttask/DividirView.java
@@ -0,0 +1,14 @@
+package com.example.intenttask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DividirView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dividir_view);
+    }
+}
---
+++ app/src/main/java/com/example/intenttask/RestarView.java
@@ -0,0 +1,14 @@
+package com.example.intenttask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestarView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restar_view);
+    }
+}
