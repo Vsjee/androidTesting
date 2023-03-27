07c7e7fa27364b399430b66b419aebd7d47a2599
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-26 20:24:48 -0500

    add go back btn and show the op res

--- app/src/main/java/com/example/intenttask/MultiplicarView.java
+++ app/src/main/java/com/example/intenttask/MultiplicarView.java
@@ -2,13 +2,37 @@ package com.example.intenttask;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MultiplicarView extends AppCompatActivity {
+
+    private Button btnGoBack;
+
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiplicar_view);
+
+        btnGoBack = findViewById(R.id.goBack);
+
+        result = findViewById(R.id.resultOp);
+
+        String res = getIntent().getStringExtra("res");
+
+        result.setText(res);
+
+        btnGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MultiplicarView.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/intenttask/RestarView.java
+++ app/src/main/java/com/example/intenttask/RestarView.java
@@ -2,13 +2,37 @@ package com.example.intenttask;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class RestarView extends AppCompatActivity {
+
+    private Button btnGoBack;
+
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restar_view);
+
+        btnGoBack = findViewById(R.id.goBack);
+
+        result = findViewById(R.id.resultOp);
+
+        String res = getIntent().getStringExtra("res");
+
+        result.setText(res);
+
+        btnGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RestarView.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
